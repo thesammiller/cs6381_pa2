077ab276b5e792274e43ffb20ecb08f51ada60df
Sam Miller <samuelsmiller@gmail.com>
2021-03-11 23:55:10 +0000

    Graphing apps added - average works, quantile broke.

--- graph_average.py
+++ graph_average.py
@@ -3,6 +3,8 @@ from mpl_toolkits import mplot3d
 import pandas as pd
 from pandas import DataFrame
 from pprint import pprint
+import os
+import time
 
 #CPU_BASE = ['whole', 'half', 'quarter']
 #CPU_DICT = {'whole': 1, 'half': 0.5, 'quarter': 0.25}
@@ -12,10 +14,15 @@ from pprint import pprint
 basename = 'map{map}_reduce{reduce}_rack{rack}'
 basefile = 'metrics/metrics_{file}.csv'
 
-FILENAMES = [basename.format(map=map, reduce=reduce, rack=rack)
-             for map in [10, 20]
-             for reduce in [2, 3]
-             for rack in [1, 2, 3]]
+#FILENAMES = [basename.format(map=map, reduce=reduce, rack=rack)
+#             for map in [10, 20]
+#             for reduce in [2, 3]
+#             for rack in [1, 2, 3]]
+
+FILENAMES = [x for x in os.listdir() if 'seconds' in x]
+#log1 = pd.read_csv('seconds_10.0.0.5.log', header=None)
+
+
 
 #FILENAMES = [basename.format(map=10, reduce=2, rack=3),
 #                basename.format(map=10, reduce=3, rack=1),
@@ -26,28 +33,34 @@ FILENAMES = [basename.format(map=map, reduce=reduce, rack=rack)
 def build_data(filenames):
     filedata = {}
     for file in filenames:
-        dfcolumns = pd.read_csv(basefile.format(file=file), nrows=1, delim_whitespace=True)
-        df = pd.read_csv(basefile.format(file=file), header=None, skiprows=1, usecols = list(range(len(dfcolumns.columns))),
-                            names=dfcolumns.columns)
+        #dfcolumns = pd.read_csv(basefile.format(file=file), nrows=1, delim_whitespace=True)
+        df = pd.read_csv(file, header=None, skiprows=1)
         filedata[file] = df
     return filedata
+
+def parse_ip(seconds_log):
+    pieces = seconds_log.split('.')
+    # seconds10, 0, 0, x, log --> we want x
+    print(pieces)
+    host = pieces[3]
+    return int(host)
 
 def main():
     data = build_data(FILENAMES)
 
-    dataSet = sorted(data.keys(), key=lambda x: str([n for n in x if n.isdigit()]))
+    dataSet = sorted(data.keys(), key=lambda x: parse_ip(x))
 
 
     #fig = plt.figure()
     #ax = fig.add_subplot(111, projection="3d")
 
     #plt.plot(xaxis, yaxis) where xaxis and yaxis are lists
-    plt.plot(dataSet, [data[f]['Total'].quantile(0.99) for f in dataSet])
+    plt.plot(dataSet, [data[f].median() for f in dataSet])
     plt.xticks(rotation=30)
+    timestamp = str(round(time.time()))[-5:]
 
-    fig = plt.figure()
-    fig.savefig("average.png")
-    plt.close(fig)
+    #plt.show()
+    plt.savefig('average_{}.png'.format(timestamp))
 
 if __name__ == '__main__':
     main()
---
+++ graph_quantile.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import pandas as pd
+from pandas import DataFrame
+from pprint import pprint
+import os
+import time
+
+
+
+FILENAMES = [x for x in os.listdir() if 'seconds' in x]
+
+
+def build_data(filenames):
+    filedata = {}
+    for file in filenames:
+        df = pd.read_csv(file, header=None, skiprows=1)
+        filedata[file] = df
+    return filedata
+
+def parse_ip(seconds_log):
+    pieces = seconds_log.split('.')
+    # seconds10, 0, 0, x, log --> we want x
+    print(pieces)
+    host = pieces[3]
+    return int(host)
+
+def main():
+    data = build_data(FILENAMES)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    for enum, file in enumerate(data):
+        zaxis = data[file].quantile(0.90)
+        ax.plot([enum]*len(data), [1, 2, 3], zaxis)
+
+        #zaxis = [dataPoints[x].quantile(0.95) for x in range(1000, 3500, 500)]
+        #ax.plot(xaxis, DIMENSIONS, [x[0] for x in zaxis[0:5]])
+
+        #zaxis = [dataPoints[x].quantile(0.99) for x in range(1000, 3500, 500)]
+        #ax.plot(xaxis, DIMENSIONS, [x[0] for x in zaxis[0:5]])
+
+    dataSet = sorted(data.keys(), key=lambda x: parse_ip(x))
+
+
+
+    timestamp = str(round(time.time()))[-5:]
+
+    fig.savefig('quantile_{}.png'.format(timestamp))
+
+
+if __name__ == '__main__':
+    main()
+
+
